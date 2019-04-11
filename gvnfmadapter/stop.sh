eb26703e56f69bf8a018f9160de53c4ff290a8f6
yangyan <yangyanyj@chinamobile.com>
2019-04-11 14:37:52 +0800

    Add https support for gvnfm
    Change-Id: I36506eb50de226275862cf4e5af30fe231bc4e71
    Issue-ID: VFC-1342
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- gvnfmadapter/stop.sh
+++ gvnfmadapter/stop.sh
@@ -13,4 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ps auxww | grep 'manage.py runserver 0.0.0.0:8484' | awk '{print $2}' | xargs kill -9
+# ps auxww | grep 'manage.py runserver 0.0.0.0:8484' | awk '{print $2}' | xargs kill -9
+ps auxww |grep 'uwsgi --http :8484 --module driver.wsgi --master' |awk '{print $2}' |xargs kill -9
