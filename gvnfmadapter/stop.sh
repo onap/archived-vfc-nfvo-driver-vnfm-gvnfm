0d476080107ec11f1f274d027dcd0b90a208b455
fujinhua <fu.jinhua@zte.com.cn>
2019-05-07 09:07:35 +0800

    Fix bug for gvnfmdriver stop sh
    Change-Id: Ia1137842b3bd34143fc92b7342aac17559d61ecc
    Issue-ID: VFC-1306
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- gvnfmadapter/stop.sh
+++ gvnfmadapter/stop.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 # ps auxww | grep 'manage.py runserver 0.0.0.0:8484' | awk '{print $2}' | xargs kill -9
-ps auxww |grep 'uwsgi --http :8484 --module driver.wsgi --master' |awk '{print $2}' |xargs kill -9
+ps auxww |grep 'uwsgi --http' |awk '{print $1}' |xargs kill -9
