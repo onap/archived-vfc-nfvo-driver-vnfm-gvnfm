9d7265d9ec45336d251c6cd44423de2d30c9be60
fujinhua <fu.jinhua@zte.com.cn>
2019-07-03 15:17:30 +0800

    Update python2 to python3
    Change-Id: Icb5a35dd320ccbf80c66089977550755810c859f
    Issue-ID: VFC-1429
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- gvnfmadapter/driver/pub/utils/restcall.py
+++ gvnfmadapter/driver/pub/utils/restcall.py
@@ -15,7 +15,7 @@
 import sys
 import traceback
 import logging
-import urllib2
+import urllib.error
 import uuid
 import httplib2
 
@@ -62,7 +62,7 @@ def call_req(base_url, user, passwd, auth_type, resource, method, content=''):
                     ret = [1, "Unable to connect to %s" % full_url, resp_status, resp_Location]
                     continue
                 raise ex
-    except urllib2.URLError as err:
+    except urllib.error.URLError as err:
         ret = [2, str(err), resp_status, resp_Location]
     except Exception as ex:
         logger.error(traceback.format_exc())
