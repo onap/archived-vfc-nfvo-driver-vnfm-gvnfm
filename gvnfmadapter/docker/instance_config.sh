20724692f07eb00b51cc215b79f069800a450bb2
yangyan <yangyanyj@chinamobile.com>
2020-02-21 16:42:12 +0800

    Modify global variable substitution
    Change-Id: I6f7e08a373babe5266dfb1af033206dff9c4827c
    Issue-ID: VFC-1595
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- gvnfmadapter/docker/instance_config.sh
+++ gvnfmadapter/docker/instance_config.sh
@@ -5,7 +5,7 @@ MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
 MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 
 if [ $MSB_IP ]; then
-    sed -i "s|MSB_SERVICE_IP.*|MSB_SERVICE_IP = '$MSB_IP'|" vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
+    sed -i "s|MSB_SERVICE_IP = .*|MSB_SERVICE_IP = '$MSB_IP'|" vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 fi
 
 if [ $MSB_PROTO ]; then
@@ -13,7 +13,7 @@ if [ $MSB_PROTO ]; then
 fi
 
 if [ $MSB_PORT ]; then
-    sed -i "s|MSB_SERVICE_PORT.*|MSB_SERVICE_PORT = '$MSB_PORT'|" vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
+    sed -i "s|MSB_SERVICE_PORT = .*|MSB_SERVICE_PORT = '$MSB_PORT'|" vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 fi
 
 if [ $SERVICE_IP ]; then
