931b316d63367e465dd0274963624d45bc477670
dyh <dengyuanhong@chinamobile.com>
2021-09-17 11:02:30 +0800

    Fix the path of config.py
    Change-Id: Ia39b1ea6f8ef7b0ffa084e9bccaa1e5d90fde87c
    Issue-ID: VFC-1886
    Signed-off-by: dyh <dengyuanhong@chinamobile.com>

--- gvnfmadapter/docker/docker-entrypoint.sh
+++ gvnfmadapter/docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Configure service based on docker environment variables
 python vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config.py
-cat vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config.py
+cat vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/driver/pub/config/config.py
 
 # microservice-specific one-time initialization
 vfc/nfvo/driver/vnfm/gvnfm/gvnfmadapter/docker/instance_init.sh
